3cd50498ff0a7d53c9547d104319ec2cf6d608b2
Ahmed Syed <153319561+aisyed425@users.noreply.github.com>
2025-10-25 18:26:04 -0400

    Update Alternative_Import_Module.py

--- Alternative_Import_Module.py
+++ Alternative_Import_Module.py
@@ -4,4 +4,4 @@ import shutil
 def move_and_arrange(desk_path, inp1, inp2):
     dst_path = os.path.join(desk_path, inp1)
     src_dst = os.chdir(desk_path)
-    lst = os.listdir()
+    lst = os.listdir(
